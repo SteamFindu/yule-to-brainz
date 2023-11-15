99fb01c800ce3d64970af69cc2cb5e6ba1f577e3
AB7090 <AB7090@student.jamk.fi>
2023-11-15 06:15:12 +0200

    testing

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-## TODO: Fill readme
+# Winamp to listenbrainz 
+
+Small rust script to add winamp logged music to listenbrainz
+
+## Usage
+
+## Later
+
+maybe make it a direct plugin to winamp instead of just reading the log files
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+#![allow(dead_code, unused)]
+
 use std::env;
+use std::fs;
+
+use glob::glob;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let files: Vec<&str> = load_filepaths(&args[1]);
+
+    dbg!(&args);
+    dbg!(&files);
+
+    // let contents = fs::read_to_string(file_path).expect("should have read file");
+
+    println!("Contents:\n{contents}");
+}
+
+fn load_filepaths(path: &str) {
+    let mut filepaths: Vec<String> = Vec::new();
+    let paths = fs::read_dir(path).unwrap();
+
+    for entry in glob("{path}").expect("failed to read glob") {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => panic!("{:?}", e),
+        }
+    }
 }
