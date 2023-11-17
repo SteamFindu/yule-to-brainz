1a2dc76fb306a9575031e94b2b145e941cc91319
AB7090 <AB7090@student.jamk.fi>
2023-11-17 04:20:44 +0200

    remake the code

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,56 @@
 
 use std::env;
 use std::fs;
+use std::fs::{DirEntry, File, ReadDir};
+use std::io;
+use std::io::Read;
+use std::time;
+use std::time::Duration;
 
-use glob::glob;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let files: Vec<&str> = load_filepaths(&args[1]);
-
-    dbg!(&args);
-    dbg!(&files);
-
-    // let contents = fs::read_to_string(file_path).expect("should have read file");
-
-    println!("Contents:\n{contents}");
+#[derive(Debug)]
+struct Log {
+    filepath: DirEntry,
+    contents: String,
 }
 
-fn load_filepaths(path: &str) {
-    let mut filepaths: Vec<String> = Vec::new();
-    let paths = fs::read_dir(path).unwrap();
+#[derive(Debug)]
+struct Song {
+    artist: String,
+    name: String,
+    length: Duration,
+    started: DateTime,
+}
 
-    for entry in glob("{path}").expect("failed to read glob") {
-        match entry {
-            Ok(path) => println!("{:?}", path.display()),
-            Err(e) => panic!("{:?}", e),
-        }
+fn main() -> Result<(), io::Error> {
+    let logs = load_files("/mnt/c/Users/sand/AppData/Roaming/WACUP/Logs/")?;
+
+    parse_logs(&logs[0]);
+
+    Ok(())
+    // let contents = fs::read_to_string(file_path).expect("should have read file");
+}
+
+fn load_files(path: &str) -> Result<Vec<Log>, io::Error> {
+    let mut files: Vec<Log> = Vec::new();
+    let paths = fs::read_dir(path).unwrap().filter_map(|e| e.ok());
+
+    for path in paths {
+        let mut file = File::open(path.path())?;
+        let mut content = String::new();
+
+        file.read_to_string(&mut content)?;
+
+        files.push(Log {
+            filepath: path,
+            contents: content,
+        });
     }
+
+    Ok(files)
+}
+
+fn parse_logs(logfile: &Log) {
+    let rows: Vec<&str> = logfile.contents.split("\n").collect();
+
+    dbg!(rows);
 }
